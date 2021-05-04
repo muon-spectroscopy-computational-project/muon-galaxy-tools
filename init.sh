2ae285d330afc6a404dcbb6aeb56ab2b732de33c
elichad <eli.chadwick.256@gmail.com>
2021-05-04 12:13:46 +0000

    add .shed.yml

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
-planemo tool_init --force \
-                  --id 'pm-muairss_write' \
+planemo tool_init --id 'pm_muairss_write' \
                   --name 'Generate muon structures (pm-muairss)' \
                   --example_command 'pm-muairss -t w Si.cell Si-muairss-castep.yaml; zip -r si-muon-airss-out.zip si-muon-airss-out' \
                   --example_input Si.cell \
@@ -9,3 +8,10 @@ planemo tool_init --force \
                   --cite_url 'https://github.com/muon-spectroscopy-computational-project/pymuon-suite/' \
                   --help_from_command 'pm-muairss'
                   #--requirement pymuon-suite@0.1.0 \
+
+planemo shed_init --category 'Computational chemistry, Molecular Dynamics' \
+                  --description 'Generate muon structures (pm-muairss)' \
+                  --long_description 'TODO' \
+                  --homepage_url 'https://github.com/muon-spectroscopy-computational-project/muon-galaxy-tools' \
+                  --remote_repository_url 'https://github.com/muon-spectroscopy-computational-project/muon-galaxy-tools/main/pm_muairss_write' \
+                  --owner 'elichad'
