12356d06f0e72d38ccddd29b1401ba0dfb227724
Jo-stfc <jyothish.thomas@stfc.ac.uk>
2021-09-16 13:00:53 +0000

    tests

--- get_out_folder.py
+++ get_out_folder.py
@@ -1,13 +1,28 @@
 import yaml
 import sys
+import os
+def filefinder(root,substr,exception='',exceptfile=''):
+    filelst=[]
+    for i in os.walk(root):
+        for j in i[2]:
+            #print(j)
+            #pdb.set_trace()
+            if substr in j and (exceptfile=='' or exceptfile not in j) and (exception=='' or exception not in i[0]):
+                #print(j)
+                filelst.append(i[0]+os.sep+j)
+    return filelst
+
 try:
     with open('params.yaml') as f:
         data=yaml.safe_load(f)
 except:
-    print('no param file')
-    sys.stdout.write('default')
+    try:
+        with open(filefinder('./','.yaml')[0]) as f:
+            data=yaml.safe_load(f)
+    except:
+        print(filefinder('./','.yaml'))
+        sys.stdout.write('default')
 try:
     sys.stdout.write(data['out_folder'])
 except Exception as e:
-    print(e)
     sys.stdout.write('default')
