6c0500127a3f882f71d38ee72c9a4fb029a61195
Jo-stfc <jyothish.thomas@stfc.ac.uk>
2021-09-17 12:15:41 +0000

    flake8 compliant

--- pm_muairss_write/get_out_folder.py
+++ pm_muairss_write/get_out_folder.py
@@ -2,22 +2,22 @@ import os
 import sys
 import yaml
 
-def filefinder(root,substr,exception='',exceptfile=''):
+
+def filefinder(root, substr, exception='', exceptfile=''):
     '''finds files with names containing substr
         within folder root and its subfolders'''
-    filelst=[]
+    filelst = []
     for i in os.walk(root):
         for j in i[2]:
-            #print(j)
-            #pdb.set_trace()
-            if substr in j and (exceptfile=='' or exceptfile not in j) and (exception=='' or exception not in i[0]):
-                #print(j)
+            if (substr in j and (exceptfile == '' or exceptfile not in j) and
+                    (exception == '' or exception not in i[0])):
                 filelst.append(i[0]+os.sep+j)
     return filelst
 
+
 try:
     with open('params.yaml') as f:
-        data=yaml.safe_load(f)
+        data = yaml.safe_load(f)
 except FileNotFoundError:
     sys.stdout.write('default')
 try:
--- pm_uep_opt_write/torelative.py
+++ pm_uep_opt_write/torelative.py
@@ -2,38 +2,41 @@ import os
 import sys
 import traceback
 
+
 def filefinder(root, substr, exception='', exceptfile=''):
     '''find files with names containing substr
         within folder root '''
-    filelst=[]
+    filelst = []
     for i in os.walk(root):
         for j in i[2]:
-            if (substr in j and (exceptfile=='' or exceptfile not in j) and
-                    (exception=='' or exception not in i[0])):
+            if (substr in j and (exceptfile == '' or exceptfile not in j) and
+                    (exception == '' or exception not in i[0])):
                 filelst.append(i[0]+os.sep+j)
     return filelst
 
+
 def replacedirs(text):
     '''replace chden_path in subfolders with cwd'''
-    spl=text.split('\n')
+    spl = text.split('\n')
     for i in range(len(spl)):
-        if spl[i][:10]=='chden_path':
-            spl[i]=spl[i][:12]+'./'
+        if spl[i][:10] == 'chden_path':
+            spl[i] = spl[i][:12]+'./'
     return '\n'.join(spl)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     try:
-        yamls=filefinder(sys.argv[1], '.yaml')
+        yamls = filefinder(sys.argv[1], '.yaml')
     except FileNotFoundError:
         print('missing argument: root folder\n')
         traceback.print_exc()
     for yamlfile in yamls:
         with open(yamlfile, 'r') as f:
-            x=f.read()
-        replaced=replacedirs(x)
+            x = f.read()
+        replaced = replacedirs(x)
         with open(yamlfile, 'w') as f:
             f.write(replaced)
-        if yamlfile==yamls[-1]:
+        if yamlfile == yamls[-1]:
             with open(yamlfile, 'r') as f:
                 print(f.read())
     print('converted to relative path')
