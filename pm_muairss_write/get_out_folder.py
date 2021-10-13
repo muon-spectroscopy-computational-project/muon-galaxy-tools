b42aa8c79c41e15114d567894da179d0892ada3c
Jo-stfc <jyothish.thomas@stfc.ac.uk>
2021-10-13 14:18:50 +0000

    cleanup

--- pm_muairss_write/get_out_folder.py
+++ pm_muairss_write/get_out_folder.py
@@ -1,27 +1,9 @@
-import os
 import sys
-
 import yaml
-
-
-def filefinder(root, substr, exception='', exceptfile=''):
-    '''finds files with names containing substr
-        within folder root and its subfolders'''
-    filelst = []
-    for i in os.walk(root):
-        for j in i[2]:
-            if (substr in j and (exceptfile == '' or exceptfile not in j) and
-                    (exception == '' or exception not in i[0])):
-                filelst.append(i[0]+os.sep+j)
-    return filelst
-
 
 try:
     with open('params.yaml') as f:
         data = yaml.safe_load(f)
-except FileNotFoundError:
-    sys.stdout.write('default')
-try:
-    sys.stdout.write(data['out_folder'])
-except KeyError:
-    sys.stdout.write('default')
+        sys.stdout.write(data['out_folder'])
+except KeyError or FileNotFoundError:
+    sys.stdout.write('muon-airss-out')
