f95a885d62287cd06e33f6a4c1af36739bb6d2ed
Jo-stfc <jyothish.thomas@stfc.ac.uk>
2021-10-13 14:43:53 +0000

    synctest

--- pm_muairss_write/get_out_folder.py
+++ pm_muairss_write/get_out_folder.py
@@ -1,9 +1,10 @@
 import sys
+
 import yaml
+
 
 try:
     with open('params.yaml') as f:
         data = yaml.safe_load(f)
-        sys.stdout.write(data['out_folder'])
 except KeyError or FileNotFoundError:
     sys.stdout.write('muon-airss-out')
