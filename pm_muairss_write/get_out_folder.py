c01f1ff756a5121517e9c9aa16506e246953f7ff
Jo-stfc <jyothish.thomas@stfc.ac.uk>
2021-10-14 09:58:00 +0000

    cleanup-inconsistencies

--- pm_muairss_write/get_out_folder.py
+++ pm_muairss_write/get_out_folder.py
@@ -6,5 +6,6 @@ import yaml
 try:
     with open('params.yaml') as f:
         data = yaml.safe_load(f)
+    sys.stdout.write(data['out_folder'])
 except KeyError or FileNotFoundError:
     sys.stdout.write('muon-airss-out')
