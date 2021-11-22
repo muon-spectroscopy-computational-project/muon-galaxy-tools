cc5f279c6e07015725a43648c98d9c9d1f2e8628
root <root@host-172-16-102-114.nubes.stfc.ac.uk>
2021-11-22 14:27:10 +0000

    uep-error-catch

--- pm_uep_opt_write/run.sh
+++ pm_uep_opt_write/run.sh
@@ -3,7 +3,13 @@
 set -o errexit
 
 for x in $1/*/*/*.yaml 
-    do 
-        pm-uep-opt $x &&
-    done || exit 1 
-    #pm-uep-opt 
+do 
+    
+    if ! out=$(pm-uep-opt $x)
+    then 
+    echo 1>&2 "$out"; exit 1
+    else
+    echo "$out"
+    fi
+done || exit
+#pm-uep-opt 
