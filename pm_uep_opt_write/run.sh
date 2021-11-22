88498de1170a5e534182c350e20f75ef656fd961
root <root@host-172-16-102-114.nubes.stfc.ac.uk>
2021-11-22 10:04:29 +0000

    uep-reduced

--- pm_uep_opt_write/run.sh
+++ pm_uep_opt_write/run.sh
@@ -1,11 +1,5 @@
 for x in $1/*/*/*.yaml 
     do 
-    pm-uep-opt $x 2> output
-    if [ $? != 0 ] 
-    then 
-        >&2 sed '/^s2/r output' <<< echo "pm-uep-opt failed:" 
-        exit 2  
-    else 
-        sed '/^s2/r output' <<< echo "pm-uep-opt successful:" 
-    fi
-    done #pm-uep-opt 
+        pm-uep-opt $x &&
+        echo "$?"
+    done || exit  #pm-uep-opt 
