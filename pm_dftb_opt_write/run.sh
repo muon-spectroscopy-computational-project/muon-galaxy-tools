90f8b82d33e6da7276c0a24b3c07ba05f20661ba
Jo-stfc <jyothish.thomas@stfc.ac.uk>
2022-01-27 09:08:25 +0000

    error_propagation

--- pm_dftb_opt_write/run.sh
+++ pm_dftb_opt_write/run.sh
@@ -1,1 +1,14 @@
-for x in $1/dftb+/* ; do (echo $x && cd $x && dftb+) ;done 
+#! /bin/bash
+
+set -o errexit
+
+for x in dftb+/* 
+do 
+    
+    if ! out=$(echo $x && cd $x && dftb+)
+    then 
+    echo 1>&2 "$out"; exit 1
+    else
+    echo "$out"
+    fi
+done || exit
