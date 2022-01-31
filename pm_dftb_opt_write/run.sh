9b9905499d1aa2b4f77f39741006e4ab23fd98ff
Jo-stfc <jyothish.thomas@stfc.ac.uk>
2022-01-31 16:57:21 +0000

    integrated_test_changes

--- pm_dftb_opt_write/run.sh
+++ pm_dftb_opt_write/run.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
 set -o errexit
-
-for x in dftb+/* 
+if [ ! -d "$1/dftb+" ]; then echo "no folder"; else echo "folder"; fi
+[ ! -d "$1/dftb+" ] && ( echo 1>&2 "no dftb+ structures found" && exit 2 )
+for x in $1/dftb+/* 
 do 
     
     if ! out=$(echo $x && cd $x && dftb+)
     then 
-    echo 1>&2 "$out"; exit 1
+    echo 1>&2 "$out"; exit 3
     else
     echo "$out"
     fi
-done || exit
+done || exit 
