d1efd96a0423b1d3f2affcbf6460bb6df762b58e
Jo-stfc <jyothish.thomas@stfc.ac.uk>
2022-02-04 15:44:37 +0000

    dois

--- pm_dftb_opt_write/run.sh
+++ pm_dftb_opt_write/run.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 set -o errexit
-if [ ! -d "$1/dftb+" ]; then echo "no folder"; else echo "folder"; fi
 [ ! -d "$1/dftb+" ] && ( echo 1>&2 "no dftb+ structures found" && exit 2 )
 for x in $1/dftb+/* 
 do 
