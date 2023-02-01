fb6939887423e1a5385cf9147b3e7aa1a10efdeb
Joel Davies <joel.davies@stfc.ac.uk>
2023-02-01 09:22:42 +0000

    Fix bug with integral y_axis and add another test

--- muspinsim_config/build_file.py
+++ muspinsim_config/build_file.py
@@ -314,13 +314,15 @@ parse_func_dict = {
     "axes_options": {
         "x_axis_options": {
             "x_axis": lambda value: build_block("x_axis", [value]),
-            "average_axes": lambda values: build_block("average_axes", values),
+            "average_axes": lambda values: build_block(
+                "average_axes", values),
         },
-        "y_axis": lambda value: build_block("y_axis", [value])
+        "x_axis": lambda value: build_block("x_axis", [value]),
+        "y_axis": lambda value: build_block("y_axis", [value]),
+        "average_axes": lambda values: build_block(
+            "average_axes", values)
     },
-    "average_axes_restricted": lambda values: build_block(
-        "average_axes", values
-    ),
+    "average_axes": lambda values: build_block("average_axes", values),
     "experiment_preset": lambda value: build_block("experiment", [value]),
     "orientations": lambda values: build_block(
         f"orientation {EULER_CONVENTION}",
@@ -354,7 +356,6 @@ parse_none_dict = {
     # Allow average_axis to be None as by default is orientation in
     # muspinsim but letting the UI present this here instead
     "average_axes": ["none"],
-    "average_axes_restricted": ["none"]
 }
 
 
